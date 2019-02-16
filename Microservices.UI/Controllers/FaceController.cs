8e6d636e6f9e7dfe9f1ab4c8c055e1082b307b6f
Daniel Araujo Gomes <daniel.araujo@live.com>
2019-02-16 11:56:36 -0200

    Implementeção chamadas moc

--- Microservices.UI/Controllers/FaceController.cs
+++ Microservices.UI/Controllers/FaceController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microservices.UI.Contracts;
+using Microservices.UI.Moc.Contratos;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Microservices.UI.Controllers
 {
@@ -15,7 +21,27 @@ namespace Microservices.UI.Controllers
         [Route("api/Face")]
         public IActionResult Index(FaceToPost face)
         {
+            var x = new Xpto(); x.DoSomething(Request.GetUri(), "User");
+
             return Ok(new FaceToProcessing());
+        }
+    }
+
+
+    public class Xpto
+    {
+        public async void DoSomething(Uri uri, string api)
+        {
+            var httpClient = new HttpClient();
+            var byteData = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new UserToPost()));
+            var content = new ByteArrayContent(byteData);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var baseUrl = uri.Scheme + Uri.SchemeDelimiter + uri.Host + ":" + uri.Port;
+
+            var response = await httpClient.PostAsync($"{baseUrl}/api/{api}", content);
+
+
         }
     }
 }
