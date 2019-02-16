f5f41139adde870e1ccef0a696edd23b044717c7
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2019-02-16 16:16:45 -0200

    Inclusão das classes (Contratos) produtos no Mock

---
+++ Microservices.UI.Moc/Contratos/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Microservices.UI.Moc.Contratos
+{
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
