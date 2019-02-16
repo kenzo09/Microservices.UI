149b5355967b92bc24f0800fe9dc10ed8846a071
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2019-02-16 11:23:19 -0200

    Inclusão do projeto Mock

---
+++ Microservices.UI.Moc/Contratos/UserToResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.UI.Moc.Contratos
+{
+    public class UserToResponse
+    {
+        public bool Processing { get; set; } = true;
+
+        public Guid UserId { get; set; } = new Guid();
+    }
+}
